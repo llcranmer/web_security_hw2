d2149c9aa445cefc02e748e90490b16fcad0e5f4
llc <touldeguiacranmer@gmail.com>
2018-10-04 16:59:12 -0500

    solved prob_2

--- HW2/HW2.java
+++ HW2/HW2.java
@@ -10,59 +10,66 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
  
 public class HW2 {    
-  static void P1() throws Exception {
-    byte[] cipherText = Files.readAllBytes(Paths.get("cipher1.txt"));
+  
+  // static void P1() throws Exception {
+  //   byte[] cipherText = Files.readAllBytes(Paths.get("cipher1.txt"));
     
-    // BEGIN SOLUTION
-    byte[] iv = new byte[] { 0, 0, 0, 0, 
-                             0, 0, 0, 0, 
-                             0, 0, 0, 0, 
-                             0, 0, 0, 0 };
-    byte[] key;
-    byte[] plainText = cipherText;    
-    // END SOLUTION
+  //   // BEGIN SOLUTION
+  //   byte[] iv = new byte[] { 0, 0, 0, 0, 
+  //                            0, 0, 0, 0, 
+  //                            0, 0, 0, 0, 
+  //                            0, 0, 0, 0 };
+  //   byte[] key;
+  //   byte[] plainText = cipherText;    
+  //   // END SOLUTION
     
-    System.out.println(new String(plainText, StandardCharsets.UTF_8));
-  }
+  //   System.out.println(new String(plainText, StandardCharsets.UTF_8));
+  // }
 
   static void P2() throws Exception {    
     // BEGIN SOLUTION
-    for (int i = 0; i < 1; i++) {
-      byte[] message = Files.readAllBytes(Paths.get(String.format("messages/plain2%d.txt", i)));
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    for (int i = 0; i < 1000; i++) {
+      byte[] message = Files.readAllBytes(Paths.get(String.format("/Users/toul/Desktop/F_2018/Web_Security/HW2/HW2/messages/plain2%d.txt", i)));
+      byte[] encodedHash = digest.digest(message);
+      if(encodedHash[0] == 3 && encodedHash[1] == 59){
+        System.out.print("2"+i);
+      }
+
     }
     // END SOLUTION
   }
 
-  static void P3() throws Exception {
-    byte[] cipherText = Files.readAllBytes(Paths.get("cipher3.txt"));
+  // static void P3() throws Exception {
+  //   byte[] cipherText = Files.readAllBytes(Paths.get("cipher3.txt"));
     
-    // BEGIN SOLUTION
-    byte[] plainText = cipherText;    
-    // END SOLUTION
+  //   // BEGIN SOLUTION
+  //   byte[] plainText = cipherText;    
+  //   // END SOLUTION
     
-    System.out.println(new String(plainText, StandardCharsets.UTF_8));
-  }
+  //   System.out.println(new String(plainText, StandardCharsets.UTF_8));
+  // }
 
-  static void P4() throws Exception {
-    byte[] iv = new byte[] { 0, 0, 0, 0, 
-                             0, 0, 0, 0, 
-                             0, 0, 0, 0, 
-                             0, 0, 0, 0 };
-    byte[] cipherText = Files.readAllBytes(Paths.get("cipher4.txt"));
+  // static void P4() throws Exception {
+  //   byte[] iv = new byte[] { 0, 0, 0, 0, 
+  //                            0, 0, 0, 0, 
+  //                            0, 0, 0, 0, 
+  //                            0, 0, 0, 0 };
+  //   byte[] cipherText = Files.readAllBytes(Paths.get("cipher4.txt"));
     
-    // BEGIN SOLUTION
-    byte[] plainText = cipherText;    
-    // END SOLUTION
+  //   // BEGIN SOLUTION
+  //   byte[] plainText = cipherText;    
+  //   // END SOLUTION
     
-    System.out.println(new String(plainText, StandardCharsets.UTF_8));
-  }
+  //   System.out.println(new String(plainText, StandardCharsets.UTF_8));
+  // }
 
   public static void main(String [] args) {
     try {  
-      P1();
-      P2();
-      P3();
-      P4();
+      //P1();
+       P2();
+      // P3();
+      // P4();
     } catch (Exception e) {
       e.printStackTrace();
     } 
